5414f495aee846c0fdcb43e0bfe2579570b75060
Pridhvi Pinninti <pridhvi.pinninti@qentelli.com>
2022-01-12 15:21:11 +0530

    i am adding sample.js

--- sample.js
+++ sample.js
@@ -1,4 +1,26 @@
 var b = 10
 var a = 20
+var c = "hi"
 sum =  a + b
+console.log(sum)
 
+function add(){
+    console.log(sum)
+}
+
+//Block scope
+if(s>2)
+{
+    let s = 33
+}
+//Hoisting
+
+//console.log(x)
+
+// x = 12 //Initialisation
+ var x // declaration
+
+ m =10
+ if (m > 1){
+     console.log(m)
+ }
